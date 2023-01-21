a50ed67cf93c6e4628d4d84df672a05bccd337ad
mikor85 <cormakoff@gmail.com>
2023-01-21 20:53:45 +0200

    Initial commit

---
+++ src/main/java/telran/project/dmitryjavatelegrambot/DmitryJavaTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package telran.project.dmitryjavatelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmitryJavaTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmitryJavaTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/project/dmitryjavatelegrambot/DmitryJavaTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.project.dmitryjavatelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmitryJavaTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
